45710e46ae9fdc6cef605bc1d0ae594d8303e476
Lorthrash <65502232+Lorthrash@users.noreply.github.com>
2020-07-29 16:40:55 -0500

    SnakePlayer Class File
    Pretty much empty file atm.

---
+++ Assets/Scripts/SnakePlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public SnakePlayer(float X, float Y, Color shade)
+    {
+
+    }
+
+    public bool CheckCollide(float X, float Y)
+    {
+
+    }
+
+    public bool CheckCollideHead(float X, float Y)
+    {
+
+    }
+
+    public Draw()
+    {
+
+    }
+
+    public Move()
+    {
+
+    }
+
+    public ChangeDir(Direction dir)
+    {
+
+    }
+
+    public Die()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
